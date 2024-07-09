cb4f73cdce6b8ad2b8e66b774e58433515404998
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-07-09 14:55:13 -0300

    feat: add /me endpoint to get current user info

--- backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/controllers/AuthenticationController.java
+++ backend/codigo-certo-fullstack/src/main/java/com/claudionetto/codigo_certo_fullstack/controllers/AuthenticationController.java
@@ -1,16 +1,19 @@
 package com.claudionetto.codigo_certo_fullstack.controllers;
 
+import com.claudionetto.codigo_certo_fullstack.dtos.mappers.UserMapper;
 import com.claudionetto.codigo_certo_fullstack.dtos.requests.UserAuthenticationRequestDTO;
 import com.claudionetto.codigo_certo_fullstack.dtos.requests.UserRegisterDTO;
 import com.claudionetto.codigo_certo_fullstack.dtos.responses.UserAuthenticationResponseDTO;
+import com.claudionetto.codigo_certo_fullstack.dtos.responses.UserResponseDTO;
 import com.claudionetto.codigo_certo_fullstack.services.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,6 +33,12 @@ public class AuthenticationController {
             @RequestBody @Valid UserAuthenticationRequestDTO request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDTO> currentUser(Principal principal){
+
+        return ResponseEntity.ok(service.getCurrentUser(principal));
     }
 
 }
